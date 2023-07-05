3e7a07eb18ef1ca2ede411f798e46fee97a931f9
Tanner Cox <tecox@hmsracing.com>
2023-07-05 13:01:53 -0400

    Migrated Changes to React Folder Structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import App from './components/App'
+import reducer from './reducers'
+import middleware from './middleware'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 )
